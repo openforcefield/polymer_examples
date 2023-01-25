f8981f4644a787feec5fb39954196bedc1d0da60
Timotej Bernat <tibe3324@colorado.edu>
2023-01-25 16:16:36 -0700

    Removed debug.py

--- debug.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-from openff.toolkit.typing.engines.smirnoff import ForceField
-
-offxml_src = Path('/home/timber/Documents/Python/openff-workspace/polymer_examples/xml examples/base_library_charges.offxml')
-ff = ForceField(str(offxml_src))
