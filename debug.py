f0a52b3cdd4a455cfa6904e49d9e17705f6f0698
Timotej Bernat <tibe3324@colorado.edu>
2023-02-02 17:35:00 -0700

    Added support for water solvation in polymer sims, created versions of all JSONs with water as additional monomers

---
+++ debug.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+from openff.toolkit.topology import Topology
+
+p = Path('packmol_solva/water/water2.pdb')
+q = Path('water.json')
+
+off_topology, _, error = Topology.from_pdb_and_monomer_info(str(p), q, strict=True, verbose=False)
---
+++ xml_examples/xml_debug.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+from openff.toolkit import ForceField
+
+offxml_src = Path.cwd()
+ff1 = ForceField(str(offxml_src/'minimal working xml.offxml')) # will load without a problem
+ff2 = ForceField(str(offxml_src/'minimal erring xml.offxml'))  # will raise DefinitionSyntaxError and fail
